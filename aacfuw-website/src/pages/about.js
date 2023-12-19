dab9f7720111ac151404948714da3872b4bb7492
David Pak <sungh1404@gmail.com>
2023-12-18 18:40:35 -0800

    Added example page layout for ease of use later. (About page, members page, example single member page)

---
+++ aacfuw-website/src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <div>
+        <h1>About Page</h1>
+        <p>Lorem impsum dolor sit amet consectetur</p>
+        <p>Lorem ipsum dolor sit amet consectetur</p>
+    </div>
+  )
+}
---
+++ aacfuw-website/src/pages/members/david.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function David() {
+  return (
+    <div>
+        <h1>David's page</h1>
+        <h2>This is an example of where we will place all of the member's pages.</h2>
+    </div>
+  )
+}
