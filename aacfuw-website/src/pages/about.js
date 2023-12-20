72fcd7c448e9c5ffa27998812dc6a30f248266f4
David Pak <sungh1404@gmail.com>
2023-12-19 20:23:03 -0800

    Added basic stuff to home page

--- aacfuw-website/src/pages/about.js
+++ aacfuw-website/src/pages/about.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
+import Layout from '../components/layout'
+
 export default function about() {
   return (
-    <div>
+    <Layout>
+      <div>
         <h1>About Page</h1>
         <p>Lorem impsum dolor sit amet consectetur</p>
         <p>Lorem ipsum dolor sit amet consectetur</p>
     </div>
+    </Layout>
   )
 }
--- aacfuw-website/src/pages/index.js
+++ aacfuw-website/src/pages/index.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container'
 
-import Layout from '../components/layout'
-import Head from '../components/head'
+import Layout from '../components/layout';
+import Head from '../components/head';
+import * as styles from '../styles/index.module.scss';
 
+import background from '../images/logo_transparent.png';
 
 function Index() {
   return (
     <Layout>
       <Head title="Home" />
+      <Container fluid className={styles.centerContainer}>
+        <h2 className={styles.smallText}>We are</h2>
+        <h1 className={styles.bigText}>AACF</h1>
+        <Button href="/about" variant="outline-secondary" className={styles.learnMoreButton}>Learn More</Button>
+      </Container>
+
+      <Container fluid className={styles.backgroundContainer}>
+
+      </Container>
     </Layout>
-  )
+  );
 }
 
 export default Index;
