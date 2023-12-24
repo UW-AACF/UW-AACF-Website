92cd22d16792a3272adb217f03189b784447a2d8
mmachida <mmachida@cs.washington.edu>
2023-12-24 01:38:50 -0800

    added working image carousel to home page

--- aacfuw-website/src/pages/index.js
+++ aacfuw-website/src/pages/index.js
@@ -4,19 +4,31 @@ import Container from 'react-bootstrap/Container'
 
 import Layout from '../components/layout';
 import Head from '../components/head';
+import Carousel from '../components/carousel';
 import * as styles from '../styles/index.module.scss';
 
-import background from '../images/logo_transparent.png';
+// import background from '../images/logo_transparent.png';
 
 function Index() {
+
   return (
     <Layout>
       <Head title="Home" />
-      <Container fluid className={styles.centerContainer}>
-        <h2 className={styles.smallText}>We are</h2>
-        <h1 className={styles.bigText}>AACF</h1>
-        <Button href="/about" variant="outline-secondary" className={styles.learnMoreButton}>Learn More</Button>
-      </Container>
+      <div>
+        <Container fluid className={styles.centerContainer}>
+          <h2 className={styles.smallText}>We are</h2>
+          <h1 className={styles.bigText}>AACF</h1>
+          <Button href="/about" variant="outline-secondary" className={styles.learnMoreButton}>Learn More</Button>
+        </Container>
+      </div>
+      <div className={styles.backgroundCarousel}>
+        <Carousel/>
+      </div>
+      <div className={styles.carouselOverlay}/>
+      <Container>
+        <h1>Hooray this works</h1>  {/* Just to show that we can put stuff after the carousel :) -mm */}
+      </Container>   
+      
 
       {/* There is no backgroundContainer style yet */}
       {/* <Container fluid className={styles.backgroundContainer}>
